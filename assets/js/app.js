1096f2555b7b6739123e823213edec51a6d46f5c
GlisseLisbeth <glisselisbeth@gmail.com>
2017-05-03 11:06:58 -0500

    add validaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,43 @@
 (function() {
-    var datename = document.getElementById('datename');
-    var dni = document.getElementById('dni');
-    var form = document.getElementById('form');
-
-    var msg = document.createElement('div');
-    msg.id = 'msg';
-    msg.className            = 'msg';
-    msg.style.display = 'none';
-
-    form.appendChild(msg);
-
-    datename.addEventListener('invalid', function(event){
+  var formulario = document.getElementById('formulario');
+  var objForm = [
+    {nameForm:document.getElementById('datename'), msgForm:document.createElement('div'), classForm:'error-datename', msgVacio:'Por favor, ingrese su nombre', msgError:'La letra de esta palabra debe empezar con mayúscula'},
+    {nameForm:document.getElementById('datelastname'), msgForm:document.createElement('div'), classForm:'error-datelastname', msgVacio:'Por favor, ingrese su apellido', msgError:'La letra de esta palabra debe empezar con mayúscula'},
+    {nameForm:document.getElementById('dni'), msgForm:document.createElement('div'), classForm:'error-dni', msgVacio:'Por favor, ingrese su DNI', msgError:'Debe contener 8 dígitos y sólo números'},
+    {nameForm:document.getElementById('date'), msgForm:document.createElement('div'), classForm:'error-date', msgVacio:'Por favor, ingrese su Fecha de Nacimiento', msgError:'Por favor, ingrese su fecha de nacimiento'},
+    {nameForm:document.getElementById('celular'), msgForm:document.createElement('div'), classForm:'error-celular', msgVacio:'Por favor, ingrese su Numero de Celular', msgError:'Por favor, ingrese su numero de celular'},
+    {nameForm:document.getElementById('fono'), msgForm:document.createElement('div'), classForm:'error-fono', msgVacio:'Por favor, ingrese su Numero de Telefono', msgError:'Por favor, ingrese su numero de telefono'},
+    {nameForm:document.getElementById('contrasenia'), msgForm:document.createElement('div'), classForm:'error-contrasenia', msgVacio:'Por favor, ingrese su Contraseña', msgError:'Debe contener al menos 6 caracteres'},
+    {nameForm:document.getElementById('confirmcontrasenia'), msgForm:document.createElement('div'), classForm:'error-confirmcontrasenia', msgVacio:'Por favor, ingrese su Contraseña', msgError:'Su contraseña no coincide con la ingresada'},
+    {nameForm:document.getElementById('coment'), msgForm:document.createElement('div'), classForm:'error-coment', msgVacio:'Por favor, ingrese su Comentario', msgError:'Debe contener al menos 10 caráteres'},
+  ]
+    objForm.forEach(function(e){
+      e.nameForm.addEventListener('invalid', function(event){
         event.preventDefault();
         if ( ! event.target.validity.valid ) {
-          datename.className    = 'invalid';
-          msg.classList.add("error");
-          msg.style.display = 'block';
-          if (datename.value.length == "0"){
-            msg.textContent   = 'Por favor, ingrese sus nombres y apellidos';
+          e.nameForm.className    = 'invalid';
+          e.msgForm.classList.add(e.classForm);
+          e.msgForm.style.display = 'block';
+          if (e.nameForm.value.length == "0"){
+            e.msgForm.textContent   = e.msgVacio;
           }
           else{
-            msg.textContent   = 'La letra de cada palabra debe empezar con mayúscula';
+            e.msgForm.textContent   = e.msgError;
+            if(e.nameForm == 'confirmcontrasenia' && e[i-1].nameForm.textContent != e.nameForm.textContent){
+              e.msgForm.textContent   = e.msgError;
+            }
           }
-      }
-    });
-
-    datename.addEventListener('input', function(event){
-        if ( 'block' === msg.style.display ) {
-            datename.className = '';
-            msg.style.display = 'none';
+          formulario.insertBefore(e.msgForm, e.nameForm.nextSibling);
         }
+      });
+
+      e.nameForm.addEventListener('input', function(event){
+        if ( 'block' === e.msgForm.style.display) {
+          e.nameForm.className = '';
+          e.msgForm.style.display = 'none';
+        }
+      });
+
     });
 
 })();
